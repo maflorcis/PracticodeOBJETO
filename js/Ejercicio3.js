c753d172f909f3ac84472a66abf63ed8e5af88a9
floralonso <floralonso@gmail.com>
2022-08-03 17:07:30 -0300

    estructura general del práctico

---
+++ js/Ejercicio3.js
@@ -0,0 +1,6 @@
+/* 3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las
+propiedades de alto y ancho, mas los métodos necesarios para modiﬁcar y
+mostrar sus propiedades, calcular el perímetro y el área*/
+
+//Área del rectángulo es A = h * b
+//Perímetro del rectángulo es P = 2 L + 2 A
